acedc13654f09a9a57bbbd41f81ea1c3c2635a83
HirenBhalani-BTC <h.o.bhalani@gmail.com>
2014-07-31 12:37:29 +0530

    sdf

--- config/application.rb
+++ config/application.rb
@@ -4,6 +4,7 @@ require File.expand_path('../boot', __FILE__)
 require "active_record/railtie"
 require "action_controller/railtie"
 require "action_mailer/railtie"
+require "active_resource/railtie"
 require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
@@ -20,7 +21,10 @@ module SsApp
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
-
+        config.assets.enabled = true
+        config.assets.version = '1.0'
+        config.assets.initialize_on_precompile = false
+        config.serve_static_assets = false
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
