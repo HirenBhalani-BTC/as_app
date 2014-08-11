91f58255c42887b1c6c2926084eb3127995670ec
HirenBhalani-BTC <h.o.bhalani@gmail.com>
2014-08-11 11:42:50 +0530

    Add following

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
-before_action :signed_in_user, only: [:edit, :update]
-before_action :correct_user, only: [:edit, :update]
-before_action :admin_user, only: :destroy
+before_action :signed_in_user,only: [:index, :edit, :update, :destroy, :following, :followers]
+before_action :correct_user, only: [:edit,:update]
+before_action :admin_user,     only: :destroy
 	def index
 		@users=User.paginate(page: params[:page])
 	end
@@ -16,11 +16,11 @@ before_action :admin_user, only: :destroy
   	end
 
   	def edit
-  		# @user = User.find(params[:id])
+  		@user = User.find(params[:id])
   	end
 
   	def update
-  		# @user = User.find(params[:id])
+  		 @user = User.find(params[:id])
   		if @user.update_attributes(user_params)
   			flash[:success]="Profile updated"
   			redirect_to @user
@@ -46,6 +46,22 @@ before_action :admin_user, only: :destroy
       flash[:success]="User deleted."
       redirect_to users_path
     end
+
+
+    def following
+      @title = "Following"
+      @user = User.find(params[:id])
+      @users = @user.followed_users.paginate(page: params[:page])
+      render 'show_follow'
+    end
+
+    def followers
+      @title = "Followers"
+      @user = User.find(params[:id])
+      @users = @user.followers.paginate(page: params[:page])
+      render 'show_follow'
+    end
+
 
   	private 
   	def user_params
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -113,12 +113,34 @@ describe 'Authentication' do
 				describe "submitting to the update action" do
 					before { patch user_path(user)}
 					specify { expect(response).to redirect_to(signin_path)}
-				end				
+				end		
+
+				 describe "visiting the following page" do
+        			before { visit following_user_path(user) }
+          			it { should have_title('Sign in') }
+        		end
+
+       			 describe "visiting the followers page" do
+          			before { visit followers_user_path(user) }
+          			it { should have_title('Sign in') }
+        		end		
 			end		
 			describe "visiting the user index" do
 				before { visit users_path}
-				# it { should have_title('Sign in')}
+				 it { should have_title('Sign in')}
 			end	
+
+			 describe "in the Relationships controller" do
+     		   describe "submitting to the create action" do
+          		before { post relationships_path }
+          		specify { expect(response).to redirect_to(signin_path) }
+        		end
+
+	       	 describe "submitting to the destroy action" do
+    	    	  before { delete relationship_path(1) }
+        	  	specify { expect(response).to redirect_to(signin_path) }
+       		 end
+       		end
 		end
 
 		describe "as wrong user" do
@@ -127,9 +149,10 @@ describe 'Authentication' do
 
 			before { sign_in user, no_capybara: true }
 
-			describe "submitting a GET request to the Users#edit action" do
+			describe "submitting a GET request to the Users#edit action",t: true do
 				before { get edit_user_path(wrong_user)}
-				specify { expect(response.body).not_to match(full_title('Edit user'))}
+				
+				 specify { expect(response.body).not_to match(full_title('Edit user'))}
 				specify { expect(response).to redirect_to(root_path)}
 			end
 
@@ -139,7 +162,7 @@ describe 'Authentication' do
 			end
 		end
 		describe "as non-admin user" do
-     		 let(:user) { FactoryGirl.create(:user) }
+     		let(:user) { FactoryGirl.create(:user) }
       		let(:non_admin) { FactoryGirl.create(:user) }
 
 	      	before { sign_in non_admin, no_capybara: true }
